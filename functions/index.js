ea5092e1c4a6c7c3b0f8d115fca644ea752c9ecc
jadekang <jkang.se@gmail.com>
2020-03-11 10:37:58 -0700

    Started to setup my cloud firestore for Bingle. To interact with it using the adminSDK locally, I obtained permission key from firebase.

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,13 @@
+var serviceAccount = require('./permissions.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://bingle-backend.firebaseio.com'
+});
+
+////// line (1) through (6) => (1) load in your permissions file and then (2) use it to initialize your application. ////
+const db = admin.firestore();
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
